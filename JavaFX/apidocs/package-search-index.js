16c17caaeb34e7941796d516a86bf75c3c96e07f
elkrzys <43821437+elkrzys@users.noreply.github.com>
2021-04-27 22:22:37 +0200

    Removed redundant apidocs directory

--- JavaFX/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.lipiec.Controllers"},{"l":"pl.lipiec.Exceptions"},{"l":"pl.lipiec.Models"},{"l":"pl.lipiec.passwordmanagerfx"},{"l":"pl.lipiec.ViewExtensions"}]
--- JavaSwing/apidocs/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.lipiec.Controllers"},{"l":"pl.lipiec.Exceptions"},{"l":"pl.lipiec.Model"},{"l":"pl.lipiec.passwordmanager"},{"l":"pl.lipiec.View"}]
